a757721bbd5d9aaa44e5cf9a16cd4d98c4a691dd
Marcos Alberto Saavedra <once.marcos.alberto@gmail.com>
2021-02-04 17:20:15 -0500

    se

--- routes/web.php
+++ routes/web.php
@@ -48,8 +48,8 @@ Route::resource('Api/ConcretoDetalles','ConcretoDetallesController');
 Route::get('Api/ConcretoDetalles/{concreto_id}/consulta_data', 'ConcretoDetallesController@consulta_data');
 Route::post('Api/ConcretoDetalles_update/{id}', 'ConcretoDetallesController@update');
 Route::get('Concreto/{concreto_id}/pdfConcreto', 'ConcretoController@pdfConcreto');
-Route::get('Concreto/{file}', function ($file) {
-    $data =Storage::disk('/Appvia40express/Concreto')->response("$file");
+Route::get('Appvia40express/Concreto/{file}', function ($file) {
+    $data =Storage::disk('/Concreto')->response("$file");
     if($data){
         return $data;
     }else{
